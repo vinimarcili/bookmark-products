6bc293c366975e72375225fd1a543343b1db4fc6
Viní Marcili <vinipetter@gmail.com>
2022-01-15 00:22:46 -0300

    fix lint

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common'
 import { MainRoutingModule } from './main.routes'
 import { SharedModule } from 'src/app/shared/shared.module'
 import { HttpClientModule } from '@angular/common/http'
-import { LoaderComponent } from 'src/app/shared/components/loader/loader.component'
 
 @NgModule({
   declarations: [],
